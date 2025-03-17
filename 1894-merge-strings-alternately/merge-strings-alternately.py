55ddf3a6ffe3a4ec121ff4d3c7bdae07e23e609a
Akshata Prakash Salunkhe <143021478+AkshataSalunkhe@users.noreply.github.com>
2025-03-17 11:43:35 -0400

    Time: 41 ms (37.91%) | Memory: 17.9 MB (8.18%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -1,5 +1,22 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
+        '''Solution1 :
+
+        result = ""
+        high = max(len(word1), len(word2))
+        low= min(len(word1), len(word2))
+        if len(word1) ==high: 
+            highstr = word1
+        else: 
+            highstr =word2
+
+        for i in range(low):
+            result+=word1[i]+word2[i]
+        result+=highstr[low:high]
+        return result
+        '''
+
+        #Solution2 :
         merged = []
         length = min (len(word1), len(word2))
         for i in range (0, length):
