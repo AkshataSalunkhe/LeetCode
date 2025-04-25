3f6663390c572e04e776dff68c2128dcc970c9bc
Akshata Prakash Salunkhe <143021478+AkshataSalunkhe@users.noreply.github.com>
2025-04-25 15:18:17 -0400

    Time: 268 ms (70.72%) | Memory: 0B (100.00%) - LeetSync

--- 613-shortest-distance-in-a-line/shortest-distance-in-a-line.sql
+++ 613-shortest-distance-in-a-line/shortest-distance-in-a-line.sql
@@ -1,6 +1,5 @@
-# Write your MySQL query statement below
-select min(dist) shortest 
-from(
-select abs(p1.x-p2.x) dist
-from point p1, point p2
-where p1.x!=p2.x) as p 
+select min(a.x-b.x) as shortest
+
+from point as a
+join point as b
+where a.x>b.x
