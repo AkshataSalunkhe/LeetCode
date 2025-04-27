40304e13b0c2cd73f37a208eaf0bb9ffd876318a
Akshata Prakash Salunkhe <143021478+AkshataSalunkhe@users.noreply.github.com>
2025-04-27 13:41:45 -0400

    Time: 2217 ms (85.60%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1688-the-most-recent-orders-for-each-product/the-most-recent-orders-for-each-product.sql
@@ -0,0 +1,10 @@
+with cte as (
+select distinct(product_id),max(order_date) over (partition by product_id )as order_date 
+from orders
+)
+
+select p.product_name, p.product_id, order_id, order_date
+from products p join orders o on p.product_id=o.product_id
+and (p.product_id, o.order_date) in (select distinct(product_id),max(order_date) over (partition by product_id )as order_date 
+from orders)
+order by p.product_name ASC, p.product_id ASC, o.order_id ASC
