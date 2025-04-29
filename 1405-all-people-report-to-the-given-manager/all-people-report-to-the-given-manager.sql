25f6aec11fec2f6005b2cdba5eb63a1d3527a779
Akshata Prakash Salunkhe <143021478+AkshataSalunkhe@users.noreply.github.com>
2025-04-29 02:38:23 -0400

    Time: 250 ms (71.36%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1405-all-people-report-to-the-given-manager/all-people-report-to-the-given-manager.sql
@@ -0,0 +1,11 @@
+select employee_id 
+from employees
+where employee_id != '1' and (manager_id=1 or manager_id in 
+    (select employee_id 
+    from employees
+    where manager_id=1 
+    or 
+     manager_id in 
+    (select employee_id 
+    from employees
+    where manager_id=1)))
