66586453d876ac2bfe2fc25c1d478c3fb43809ad
Akshata Prakash Salunkhe <143021478+AkshataSalunkhe@users.noreply.github.com>
2025-04-30 20:37:28 -0400

    Time: 670 ms (49.01%) | Memory: 0B (100.00%) - LeetSync

--- 2024-calculate-special-bonus/calculate-special-bonus.sql
+++ 2024-calculate-special-bonus/calculate-special-bonus.sql
@@ -1,4 +1,6 @@
-
-select employee_id, if (employee_id % 2 = 1 and name not like 'M%', salary, 0) as bonus
-from Employees
+select employee_id, 
+case when employee_id%2=1 and name not like 'M%' then salary
+else 0
+ENd as bonus
+from employees
 order by 1
