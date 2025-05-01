8dc3a78a4848b05d226081d97db9d8e668272def
Akshata Prakash Salunkhe <143021478+AkshataSalunkhe@users.noreply.github.com>
2025-04-30 20:32:33 -0400

    Time: 589 ms (59.40%) | Memory: 0B (100.00%) - LeetSync

--- 183-customers-who-never-order/customers-who-never-order.sql
+++ 183-customers-who-never-order/customers-who-never-order.sql
@@ -1,4 +1,7 @@
-# Write your MySQL query statement below
-select name as Customers
-from Customers
-where id not in ( select customerId from Orders) 
+
+
+SELECT name AS Customers 
+FROM Customers 
+LEFT JOIN Orders
+ON Customers.id = Orders.customerId
+where Orders.customerId IS NULL;
